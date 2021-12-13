c1ed53121cd0ba3bc7ab2911d13da721d3029dab
PROxZIMA <pratikbpingale9075@gmail.com>
2021-12-13 18:33:09 +0530

    Fix: 2021 - Day - 12

--- 2021/Day 12/day-12.py
+++ 2021/Day 12/day-12.py
@@ -1,6 +1,7 @@
 import os
 from collections import defaultdict
 
+
 class Graph:
     def __init__(self):
         self.graph = defaultdict(list)
@@ -9,54 +10,52 @@ class Graph:
     def addEdge(self, u, v):
         self.graph[u].append(v)
 
-    def printAllPathsUtil(self, u, desti, visited, path):
-        if u.islower():
-            visited[u]= True
-        path.append(u)
+    def printAllPathsUtil(self, u, desti, visited, visit_twice, visit_small):
+        new_visited = visited.copy()
 
         if u == desti:
-            self.paths_count += 1
-            print(' '.join(path))
-        else:
-            for i in self.graph[u]:
-                if visited[i] == False:
-                    self.printAllPathsUtil(i, desti, visited, path)
+            return 1
 
-        path.pop()
-        visited[u] = False
+        new_visited[u] += 1
+        self.paths_count = 0
 
+        for i in self.graph[u]:
 
-    def printAllPaths(self, source, desti):
-        visited = defaultdict(lambda: False)
-        path = []
-        self.printAllPathsUtil(source, desti, visited, path)
+            if i.isupper() or (i not in new_visited):
+                self.paths_count += self.printAllPathsUtil(i, desti, new_visited, visit_twice, visit_small)
+
+            elif visit_twice and not visit_small and (i in new_visited) and new_visited[i] < 2:
+                self.paths_count += self.printAllPathsUtil(i, desti, new_visited, visit_twice, True)
+
+        return self.paths_count
+
+    def printAllPaths(self, source, desti, visit_twice):
+        visited = defaultdict(lambda: 0)
+        self.printAllPathsUtil(source, desti, visited, visit_twice, False)
+
         return self.paths_count
 
 
-def part1(data):
+def both_part(data):
     g = Graph()
+
     for entry in data:
         x, y = entry.split('-')
-        g.addEdge(x, y)
-        g.addEdge(y, x)
-    return g.printAllPaths('start', 'end'), False
+        if y != 'start' and x != 'end':
+            g.addEdge(x, y)
+        if x != 'start' and y != 'end':
+            g.addEdge(y, x)
 
-
-def part2(data):
-    g = Graph()
-    for entry in data:
-        x, y = entry.split('-')
-        g.addEdge(x, y)
-        g.addEdge(y, x)
-    print(g.printAllPaths('start', 'end', True))
-    return 36
+    return (
+        g.printAllPaths('start', 'end', False),
+        g.printAllPaths('start', 'end', True)
+    )
 
 
 def testcase():
     caves_map = open(os.path.join(os.path.dirname(__file__), "day-12-test.txt")).read().split('\n')
 
-    # assert part1(caves_map) == 10
-    assert part2(caves_map) == 36
+    assert both_part(caves_map) == (10, 36)
 
 
 if __name__ == "__main__":
@@ -64,5 +63,4 @@ if __name__ == "__main__":
 
     caves_map = open(os.path.join(os.path.dirname(__file__), "day-12-input.txt")).read().split('\n')
 
-    # print(f"Part 1: {part1(caves_map)}")
-    # print(f"Part 2: {part2(caves_map)}")
+    print('Part 1: {0}\nPart 2: {1}'.format(*both_part(caves_map)))
