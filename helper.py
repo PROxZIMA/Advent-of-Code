3afe32bb094f612651c9f41976722ac49872989a
PROxZIMA <pratikbpingale9075@gmail.com>
2022-06-16 17:17:20 +0530

    Update: Helper file

--- helper.py
+++ helper.py
@@ -2,7 +2,9 @@ import functools
 import os
 import time
 
-barebone = """import os, sys, time
+boilerplate = """import os
+import sys
+import time
 
 
 def part1(data: list[str]):
@@ -18,26 +20,32 @@ def both_part(data: list[str]):
 
 
 def testcase():
-    instructions = open(os.path.join(os.path.dirname(__file__), "day-{}-test.txt")).read()
-    instructions = instructions.split('\\n')
+    instructions = open(
+        os.path.join(os.path.dirname(__file__), "day-{}-test.txt")
+    ).read()
+    instructions = instructions.split("\\n")
 
     part1(instructions)
     # assert part1(instructions) ==
     # assert part2(instructions) ==
     # assert both_part(instructions) == (, )
 
+
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         testcase()
 
-    instructions = open(os.path.join(os.path.dirname(__file__), "day-{}-input.txt")).read()
-    instructions = instructions.split('\\n')
+    instructions = open(
+        os.path.join(os.path.dirname(__file__), "day-{}-input.txt")
+    ).read()
+    instructions = instructions.split("\\n")
 
     s = time.perf_counter()
     # print(f'Part 1: {{part1(instructions)}}')
     # print(f'Part 2: {{part2(instructions)}}')
     # print('Part 1: {{0}}\\nPart 2: {{1}}'.format(*both_part(instructions)))
-    print(f'> {{os.path.basename(__file__)}} {{round(time.perf_counter() - s, 7)}} secs')"""
+    print(f"> {{os.path.basename(__file__)}} {{round(time.perf_counter() - s, 7)}} secs")
+"""
 
 
 def timer(func):
@@ -54,16 +62,16 @@ def timer(func):
 
 
 def main():
-    year = "2023"
+    year = "2022"
     os.makedirs(year)
     os.chdir(f"{os.path.dirname(__file__)}/{year}")
 
     for i in range(1, 26):
-        dirs = f"Day_{i}"
+        dirs = f"Day_{i:02}"
         os.makedirs(dirs)
         open(f"{dirs}/day-{i}-input.txt", "w").close()
         open(f"{dirs}/day-{i}-test.txt", "w").close()
-        open(f"{dirs}/day-{i}.py", "w").write(barebone.format(i, i))
+        open(f"{dirs}/day-{i}.py", "w").write(boilerplate.format(i, i))
 
 
 if __name__ == "__main__":
